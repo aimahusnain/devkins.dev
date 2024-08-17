ddc44d7d7dedc31afd6f9966386396aaa9bf8a7c
Muhammad Husnain <aimahusnain@gmail.com>
2024-08-17 12:42:14 +0500

    Add missing artibutes

--- src/app/(routes)/services/ui-ux-design-for-web/page.tsx
+++ src/app/(routes)/services/ui-ux-design-for-web/page.tsx
@@ -3,7 +3,7 @@ import ServiceComponent from '@/components/ServiceComponent'
 
 const uiuxdesignforweb = () => {
   return (
-    <div><ServiceComponent title='ui/ux Design for web' description='sdfklj' serviceDescription="serviceDescription" /></div>
+    <div><ServiceComponent title='ui/ux Design for web' description='sdfklj' serviceDescription="serviceDescription" Service1={'1'} Service2={'2'} Service3={'3'} Service4={'4'} Service5={'5'} /></div>
   )
 }
 
